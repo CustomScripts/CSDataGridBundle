88f55d2883d49346fda7a76079f16d58e8e412b0
Pierre du Plessis <pierre@pcservice.co.za>
2012-09-05 09:33:06 +0200

    Added callback to column and added missing docblocs

--- src/CS/DataGridBundle/Grid/Column/Column.php
+++ src/CS/DataGridBundle/Grid/Column/Column.php
@@ -16,22 +16,86 @@ class Column {
 	 */
 	protected $name;
 
-	public function __construct($name)
+	/**
+	 * Sets a callback for the specific column's value
+	 * @var Closure
+	 */
+	protected $callback;
+
+	/**
+	 * Constructor to create a new Column
+	 *
+	 * @param string $name
+	 * @param mixed $callback
+	 */
+	public function __construct($name, $callback = null)
 	{
-		$this->label = $name;
 		$this->name = $name;
+
+		$this->setLabel($name)
+		    ->setCallback($callback);
 	}
 
+	/**
+	 * Verify if the column has a callback and that it is valid
+	 */
+	public function hasCallback()
+	{
+	    return $this->callback && is_callable($this->callback);
+	}
+
+	/**
+	 * Calls the callback on the column
+	 *
+	 * @param mixed $value
+	 */
+	public function callback($value)
+	{
+	    if(!$this->hasCallback())
+	    {
+	        throw new \Exception(sprintf('The column %s does\'t have a valid callback', $this->getName()));
+	    }
+
+	    return call_user_func($this->callback, $value);
+	}
+
+	/**
+	 * Sets the callback on the current column
+	 *
+	 * @param mixed $callback
+	 */
+	public function setCallback($callback)
+	{
+        $this->callback = $callback;
+
+        return $this;
+	}
+
+	/**
+	 * Return the label for the current column
+	 *
+	 * @return string
+	 */
 	public function getLabel()
 	{
 		return $this->label;
 	}
 
+	/**
+	 * Returns the name of the column
+	 *
+	 * @return string
+	 */
 	public function getName()
 	{
 	    return $this->name;
 	}
 
+	/**
+	 * Sets the label for the current column
+	 *
+	 * @param string $label
+	 */
 	public function setLabel($label)
 	{
 	    $this->label = $label;
@@ -39,8 +103,13 @@ class Column {
 	    return $this;
 	}
 
+	/**
+	 * Returns the current column as a string
+	 *
+	 * @return string
+	 */
 	public function __toString()
 	{
-	    return $this->getLabel();
+	    return $this->getName();
 	}
 }
