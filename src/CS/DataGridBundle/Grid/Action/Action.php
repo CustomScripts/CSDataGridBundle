dbc103ed65dd6fadb3632ddfb8bfa1cf898decb4
Pierre du Plessis <pierre@pcservice.co.za>
2012-09-21 09:51:48 +0200

    Added license information to all files

--- src/CS/DataGridBundle/Grid/Action/Action.php
+++ src/CS/DataGridBundle/Grid/Action/Action.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the CSDataGridBundle package.
+ *
+ * (c) Pierre du Plessis <info@customscripts.co.za>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace CS\DataGridBundle\Grid\Action;
 
 use Gedmo\Exception\InvalidArgumentException;
--- src/CS/DataGridBundle/Grid/GridInterface.php
+++ src/CS/DataGridBundle/Grid/GridInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the CSDataGridBundle package.
+ *
+ * (c) Pierre du Plessis <info@customscripts.co.za>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace CS\DataGridBundle\Grid;
 
 use CS\DataGridBundle\Grid\Filer\FilterCollection;
