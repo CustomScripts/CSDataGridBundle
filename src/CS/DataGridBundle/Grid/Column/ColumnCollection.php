34292388c9be5641d318dd1279497f8e8b2c9d28
Pierre du Plessis <pierre@pcservice.co.za>
2013-01-21 15:04:21 +0200

    Modified ColumnCollection to use Collection class

--- src/CS/DataGridBundle/Grid/Column/ColumnCollection.php
+++ src/CS/DataGridBundle/Grid/Column/ColumnCollection.php
@@ -11,78 +11,22 @@
 
 namespace CS\DataGridBundle\Grid\Column;
 
-use CS\DataGridBundle\Util\ArrayStack;
+use CS\DataGridBundle\Grid\Collection;
 
-class ColumnCollection extends ArrayStack
+class ColumnCollection extends Collection
 {
     /**
      * Adds a new Column to the collection
+     *
      * @param string  $label
      * @param integer $priority
      */
     public function add($label, $priority = 0)
     {
-        $this->_data[$priority] = new Column($label);
+    	// TODO: get column  class from configuration
+    	$column = new Column ($label);
 
-        return $this;
-    }
-
-    /**
-     * Removes a column from from the grid
-     *
-     * @param mixed $columns
-     */
-    public function remove($columns)
-    {
-        $columns = is_array($columns) ? $columns : arraya($columns);
-
-        if (count($this->_data) > 0) {
-            foreach ($this->_data as $key => $data) {
-                foreach ($columns as $label) {
-                    if (strtolower($data->getLabel()) === strtolower($label)) {
-                        unset($this->_data[$key]);
-                    }
-                }
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * Moves a column to another position
-     *
-     * @param  string           $label
-     * @param  integer          $position
-     * @return ColumnCollection
-     */
-    public function move($label, $position)
-    {
-        if (count($this->_data) > 0) {
-            foreach ($this->_data as $key => $data) {
-                if (strtolower($data->getLabel()) === strtolower($label)) {
-                    unset($this->_data[$key]);
-                    $this->_data[$position] = $data;
-                }
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * Adds columns recursively
-     *
-     * @param  array            $data
-     * @return ColumnCollection
-     */
-    public function addRecursive($data = array())
-    {
-        if (is_array($data) && !empty($data)) {
-            foreach ($data as $key => $column) {
-                $this->add($column, $key);
-            }
-        }
+    	$this->offsetSet($priority, $column);
 
         return $this;
     }
